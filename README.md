575cae2a1043f510897dfb6b2f93377fdf493c5f
Jim Ovens <you@example.com>
2017-08-12 08:45:59 +0100

    Refactor app so file is only opened once

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nc-express
 Playground for representing netcdf file via node express web app
+
+## Routes
+```
+/                                 // list files on directory path
+/:file                            // contents of :file (found on dir path)
+/:file/head(er)?(s)?              // header information of :file
+/:file/head(er)?(s)?.:header      // specific :header information of :file
+/:file/var(iable)?(s)?            // variables metadata of :file
+/:file/var(iable)?(s)?/:variable  // data associated with :variable
+```
--- netcdf-reader/app.js
+++ netcdf-reader/app.js
@@ -1,50 +1,44 @@
-const fs = require('fs');
 const express = require('express')
-
 const nc = require('./netcdf/netcdf')
-const file = 'resources/netcdf-data/sresa1b_ncar_ccsm3-example.nc';
+const filePath = 'resources/netcdf-data/';
 
 var app = express()
 
-var filePath = 'resources/netcdf-data/';
-
-var data;// = nc.openFile(file);
-
-
+// Once a file is first read from disk, maintain contents in a map
+// so each subsequent call doesn't reopen file.
+var openFiles = {};
+var openFile = (fileName) => {
+    if (!openFiles[fileName]) {
+        openFiles[fileName] = nc.openFile(filePath+fileName);
+    }
+}
+// Workaround browser asking for favicon
 app.get('/favicon.ico', function(req, res) {
     res.status(204);
 });
-
+// List file in the directory path
 app.get('/', function (req,res) {
     res.send(nc.getFiles(filePath) );
 })
-
+// Get the entire file and render as json
 app.get('/:file', function (req,res) {
-    data = nc.openFile(filePath+req.params.file);
-    res.send(data);
+    openFile(req.params.file);
+    res.send(openFiles[req.params.file]);
 })
-
+// Get the headers and optionally drill in to each one level with dot notation
 app.get('/:file/head(er)?(s)?(.:header)?', function (req, res) {
-    data = nc.openFile(filePath+req.params.file);
-    (req.params.header) ? res.send(nc.getHeaderElement(data,req.params.header)): res.send(nc.getHeader(data)) ;
+    openFile(req.params.file);
+    (req.params.header) ?
+        res.send(nc.getHeaderElement(openFiles[req.params.file],req.params.header)):
+            res.send(nc.getHeader(openFiles[req.params.file])) ;
 });
-
+// Return specific variables, list vars from header if not passed in path
 app.get('/:file/var(iable)?(s)?(/:variable)?', function (req, res) {
-    data = nc.openFile(filePath+req.params.file);
-    (req.params.variable) ? res.send(nc.getDataVariable(data,req.params.variable)): res.send(nc.getHeaderElement(data,'variables'));
+    openFile(req.params.file);
+    (req.params.variable) ?
+        res.send(nc.getDataVariable(openFiles[req.params.file],req.params.variable)):
+            res.send(nc.getHeaderElement(openFiles[req.params.file],'variables'));
 })
 
-app.listen(3000)
-
-// 
+app.listen(3000) 
  
-// // http://www.unidata.ucar.edu/software/netcdf/examples/files.html 
-// const file = 'resources/netcdf-data/sresa1b_ncar_ccsm3-example.nc';
-
-// var data = nc.openFile(file);
-// var header = nc.getHeader(data);
-// var dimensions = nc.getHeaderElement(data,'dimensions');
-// var globalAttributes = nc.getHeaderElement(data, 'globalAttributes');
-// var variables = nc.getHeaderElement(data, 'variables');
-
-// console.log(globalAttributes);
